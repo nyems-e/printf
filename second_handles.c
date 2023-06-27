a87b962b64ec6fc9a6f98893b0aa72b67bd581fc
EmmanuelAdu <christorenzo20@gmail.com>
2023-06-26 17:28:37 -0700

    This checks and prints binary

--- second_handles.c
+++ second_handles.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * handle_binary - this function converts and prints an int in the binary
@@ -9,13 +10,14 @@
 
 int handle_binary(int number)
 {
-	int count = 0;
-	char store_remainder[32]; /* char to store remainder after the division */
+	int *store_remainder = malloc(sizeof(int) * 32);
 	int index = 0; /* index to increase the iterating to store the remainder */
 	int i;
+	int count = 0;
 
 	while (number > 0)
 	{
+		store_remainder[index] = number % 2;
 		store_remainder[index] = '0' + (number % 2);
 		number /= 2;
 		index++;
@@ -27,10 +29,10 @@ int handle_binary(int number)
 	{
 		for (i = index - 1; i >= 0; i--)
 		{
-
 			_putchar(store_remainder[i]);
 			count++;
 		}
 	}
+	free(store_remainder);
 	return (count);
 }
