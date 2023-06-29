3e73bc189b67410c0edbd82390fffba8281b2627
EmmanuelAdu <christorenzo20@gmail.com>
2023-06-29 13:35:28 -0700

    This is our first commit

--- task0.c
+++ task0.c
@@ -41,6 +41,7 @@ int _printf(const char *format, ...)
 	va_list param; /* name of list */
 	int count = 0;
 	int i = 0;
+
 	va_start(param, format);
 
 	if (format == NULL || strcmp(format, " ") == 0)
